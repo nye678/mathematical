e53df52723aaeec30dc26791603dc365a031f5d1
Jake Robbins <nye678@gmail.com>
2014-02-12 08:58:16 -0500

    First Draft of meta matrix multiplication
    Requires 2 meta expressions, one to iterate through every cell in the
    matrix and the second to perform dot products on rows and columns of
    each matrix.

--- include/Matrix.h
+++ include/Matrix.h
@@ -10,24 +10,51 @@ namespace mathematical {
 		template <int N, typename T>
 		class matNT {
 			template <int I>
-			static inline matrixMulti(matNT &lhs, const matNT &rhs) {
+			static inline T matrixDot(matNT &lhs, const matNT &rhs, size_t row, size_t col)
+			{
+				return lhs[row + I * N] * rhs[col + I] + matrixDot<I - 1>(lhs, rhs, row, col);
+			}
+
+			template <>
+			static inline T matrixDot<0>(matNT &lhs, const matNT &rhs, size_t row, size_t col)
+			{
+				return lhs[row] * rhs[col];
+			}
+
+			template <int I>
+			static inline void matrixMulti(matNT &lhs, const matNT &rhs) {
 				size_t row = I % N;
-				size_t col = I / N;
-				T dot = lhs[row] * rhs[col] + lhs[row + N] * rhs[col] + lhs[row + N + N]
+				size_t col = (I / N) * N;
+				T dot = matrixDot<N - 1>(lhs, rhs, row, col);
+				matrixMulti<I - 1>(lhs, rhs);
+				lhs[I] = dot;
+			}
+
+			template <>
+			static inline void matrixMulti<0>(matNT &lhs, const matNT &rhs) {
+				lhs[0] = matrixDot<N - 1>(lhs, rhs, 0, 0);
 			}
 
 			typedef ArithmeticOps<T>	arith;
 			typedef BooleanOps<T>		boolean;
-			typedef MetaExp<vecNT, T>	meta;
+			typedef MetaExp<matNT, T>	meta;
 		protected:
 			T _mat[N*N];
 
 		public:
-			matNT &operator += (const matNT &other) { meta::execute<N - 1>(*this, other, arith::opPlus);  return *this; }
-			matNT &operator -= (const matNT &other) { meta::execute<N - 1>(*this, other, arith::opMinus); return *this; }
-			matNT &operator *= (const matNT &other);
+			matNT &operator += (const matNT &other) { meta::execute<N*N - 1>(*this, other, arith::opPlus);  return *this; }
+			matNT &operator -= (const matNT &other) { meta::execute<N*N - 1>(*this, other, arith::opMinus); return *this; }
+			matNT &operator *= (const matNT &other) { matrixMulti<N*N - 1>(*this, other); return *this; }
 
 			static matNT identity();
+
+			T &operator[] (size_t index) { return _mat[index]; }
+			const T operator[] (size_t index) const { return _mat[index]; }
+
+			matNT()
+			{
+				memset(&_mat, 0, N*N*sizeof(T));
+			}
 		};
 
 	}
